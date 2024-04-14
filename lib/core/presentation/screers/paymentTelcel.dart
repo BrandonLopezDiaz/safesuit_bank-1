5031d571e87c6affe0a28435adb66849f7c0a255
MrPegasus194 <formal_phoenix2000@hotmail.com>
2024-04-13 22:54:38 -0500

    Servicios casi terminado

---
+++ lib/core/presentation/screers/paymentTelcel.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+// Inicializo mi variable global de notificaciones
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void initializeNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('launch_background');
+  final InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
+
+Future<void> _showNotification() async {
+  flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.requestNotificationsPermission();
+
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails('1', 'SafesuitBank',
+          importance: Importance.max, priority: Priority.high);
+
+  const NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics);
+
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    'Transferencia',
+    'Haz hecho una transferencia',
+    platformChannelSpecifics,
+    payload: 'payload',
+  );
+}
+
+class PayTelcelScreen extends StatelessWidget {
+
+  Future<void> showAlertDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // El usuario debe tocar un botón para cerrar el diálogo
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Notificación enviada'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Haz hecho una transferencia.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Aceptar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Transferir pago',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 25,
+            color: Color.fromARGB(255, 242, 244, 250),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 66, 79, 120),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Monto a pagar',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 66, 79, 120),
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                '\$567.00',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 66, 79, 120),
+                  fontSize: 64,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                'Fecha límite de pago:',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 66, 79, 120),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 1),
+              Text(
+                '15 - Abril - 2024',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 66, 79, 120),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                'Periodo: Abril-2024',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 66, 79, 120),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 25.0),
+              ElevatedButton(
+                onPressed: () {
+                  print('Cantidad ingresada: \$567.00');
+                  _showNotification();
+                  showAlertDialog(context); // Mostrar el diálogo
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Color.fromARGB(255, 242, 244, 250),
+                  backgroundColor: Colors.green,
+                  minimumSize: Size(150, 50),
+                ),
+                child: Text(
+                  'Enviar',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 50.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/core/presentation/screers/serviceCFE.dart
+++ lib/core/presentation/screers/serviceCFE.dart
@@ -1,28 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:safesuit_bank/core/presentation/screers/home.dart';
+import 'package:safesuit_bank/core/presentation/screers/paymentCFE.dart';
 
 class TransCFE extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Transferir',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: TransferenciasPage(),
-    );
-  }
-}
-
-class TransferenciasPage extends StatefulWidget {
-  @override
-  _TransferenciasPageState createState() => _TransferenciasPageState();
-}
-
-class _TransferenciasPageState extends State<TransferenciasPage> {
-  TextEditingController cantidadController =
-      TextEditingController(text: "CFE");
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,74 +14,85 @@ class _TransferenciasPageState extends State<TransferenciasPage> {
               fontSize: 28, color: Color.fromARGB(255, 242, 244, 250)),
         ),
         backgroundColor: Color.fromARGB(255, 66, 79, 120),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
-      //backgroundColor: Color.fromARGB(255, 56, 99, 228),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: cantidadController,
-              textAlign: TextAlign.center,
-              keyboardType:
-                  const TextInputType.numberWithOptions(decimal: true),
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-              ),
-              style: const TextStyle(
-                  fontSize: 100.0, color: Color.fromARGB(255, 66, 79, 120)),
-            ),
-            SizedBox(height: 25.0),
-            SizedBox(
-              width: 300,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 205, 205, 205),
-                  hintText: 'No. de Servicio',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none),
-                  //border: InputBorder.none, // Quita el borde del TextField
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 25.0),
+                Center(
+                  child: Image.asset(
+                    'assets/images/logo_cfe.jpg', // Asegúrate de que el path a la imagen es el correcto.
+                    width: 150.0,
+                    height: 150.0,
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 25.0),
-            SizedBox(
-              width: 300,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 205, 205, 205),
-                  hintText: 'Importe',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none),
-                  //border: InputBorder.none, // Quita el borde del TextField
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                SizedBox(height: 25.0),
+                SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 205, 205, 205),
+                      hintText: 'No. de Servicio',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(height: 25.0),
+                SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 205, 205, 205),
+                      hintText: 'Importe',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => PayCFEScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Color.fromARGB(255, 242, 244, 250),
+                    backgroundColor: Color.fromARGB(255, 66, 79, 120),
+                    minimumSize: Size(150, 50),
+                  ),
+                  child: Text(
+                    'Continuar',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(height: 150.0),
+              ],
             ),
-            SizedBox(height: 25.0),
-            ElevatedButton(
-              onPressed: () {
-
-              },
-              style: ElevatedButton.styleFrom(
-                  foregroundColor: Color.fromARGB(255, 242, 244, 250),
-                  backgroundColor: Color.fromARGB(255, 66, 79, 120),
-                  minimumSize: Size(150, 50)),
-              child: Text(
-                'Continuar',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            SizedBox(height: 150.0),
-          ],
+          ),
         ),
       ),
     );
