761ca39515c846aff99566aa9aaaf3971ebe880b
Angel <angel5764.zs@gmail.com>
2024-05-19 17:43:25 -0600

    Entity y json retiro

---
+++ lib/core/domain/entities/retiroqr.dart
@@ -0,0 +1,11 @@
+class RetiroEntity{
+  final String username;
+  final double cantRetirar;
+  final double saldoDisponible;
+
+  RetiroEntity ({
+    required this.username,
+    required this.cantRetirar,
+    required this.saldoDisponible,
+  });
+}
